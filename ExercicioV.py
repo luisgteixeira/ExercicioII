485c2cc6bf57e7d7d8182c7ba1c1a264c8cc5087
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-07-19 10:25:58 -0300

    Implementação do exercício V finalizada.

--- ExercicioV.py
+++ ExercicioV.py
@@ -66,7 +66,7 @@ def main():
                 f4 = funcao_trapezoidal(universo_discurso, 3.0, 4.0, 5.0, 7.0)
                 f5 = funcao_gaussiana(universo_discurso, 1.0, 3.0)
 
-                # PARA AS LETRAS b) E c) ------------------------------------- #
+                # PARA AS LETRAS b), c), d) E e) ----------------------------- #
                 if pontos == 50.0:
                     universo_discurso_letra_b = universo_discurso
                     conjunto_fuzzy_letra_b = uniao(f1, f2, False)
@@ -109,11 +109,11 @@ def main():
                     universo_discurso_letra_d = universo_discurso
                     conjunto_fuzzy_letra_d = {}
 
-                    conjunto_fuzzy_letra_d['f1'] = complemento(f1, 0.0, 0.0, 1.0, False)
-                    conjunto_fuzzy_letra_d['f2'] = complemento(f2, 0.0, 0.0, 1.0, False)
-                    conjunto_fuzzy_letra_d['f3'] = complemento(f3, 0.0, 0.0, 1.0, False)
-                    conjunto_fuzzy_letra_d['f4'] = complemento(f4, 0.0, 0.0, 1.0, False)
-                    conjunto_fuzzy_letra_d['f5'] = complemento(f5, 0.0, 0.0, 1.0, False)
+                    conjunto_fuzzy_letra_d['f1'] = complemento(f1, 0.0, 0.0, 1.0, False)['padrao']
+                    conjunto_fuzzy_letra_d['f2'] = complemento(f2, 0.0, 0.0, 1.0, False)['padrao']
+                    conjunto_fuzzy_letra_d['f3'] = complemento(f3, 0.0, 0.0, 1.0, False)['padrao']
+                    conjunto_fuzzy_letra_d['f4'] = complemento(f4, 0.0, 0.0, 1.0, False)['padrao']
+                    conjunto_fuzzy_letra_d['f5'] = complemento(f5, 0.0, 0.0, 1.0, False)['padrao']
 
 
                     universo_discurso_letra_e = universo_discurso
@@ -136,58 +136,58 @@ def main():
                     conjunto_fuzzy_letra_e['geometrica'] = agregacao(conjunto_fuzzy_letra_e['geometrica'], f5, False)['geometrica']
                     conjunto_fuzzy_letra_e['minimo'] = agregacao(conjunto_fuzzy_letra_e['minimo'], f5, False)['minimo']
                     conjunto_fuzzy_letra_e['maximo'] = agregacao(conjunto_fuzzy_letra_e['maximo'], f5, False)['maximo']
-                # PARA AS LETRAS b) E c) ------------------------------------- #
+                # PARA AS LETRAS b), c), d) E e) ----------------------------- #
 
-                # titulo = str(int(pontos)) + ' pontos de discretizacao'
-                #
-                # grau_pertinencia = [[], [], [], [], []]
-                #
-                # for c in universo_discurso:
-                #     grau_pertinencia[0].append(f1[str(c)])
-                #     grau_pertinencia[1].append(f2[str(c)])
-                #     grau_pertinencia[2].append(f3[str(c)])
-                #     grau_pertinencia[3].append(f4[str(c)])
-                #     grau_pertinencia[4].append(f5[str(c)])
-                #
-                # desenha_grafico(grau_pertinencia)
+                titulo = str(int(pontos)) + ' pontos de discretizacao'
+
+                grau_pertinencia = [[], [], [], [], []]
+
+                for c in universo_discurso:
+                    grau_pertinencia[0].append(f1[str(c)])
+                    grau_pertinencia[1].append(f2[str(c)])
+                    grau_pertinencia[2].append(f3[str(c)])
+                    grau_pertinencia[3].append(f4[str(c)])
+                    grau_pertinencia[4].append(f5[str(c)])
+
+                desenha_grafico(grau_pertinencia)
             # ---------------------------- #
-            #
-            # # Letra b) ------------------- #
-            # chaves = conjunto_fuzzy_letra_b.keys()
-            #
-            # for c in chaves:
-            #     titulo = 'S-norma ' + c
-            #     grau_pertinencia = [[]]
-            #     for u in universo_discurso_letra_b:
-            #         grau_pertinencia[0].append(conjunto_fuzzy_letra_b[c][str(u)])
-            #
-            #     desenha_grafico(grau_pertinencia, universo_discurso_letra_b)
+
+            # Letra b) ------------------- #
+            chaves = conjunto_fuzzy_letra_b.keys()
+
+            for c in chaves:
+                titulo = 'S-norma ' + c
+                grau_pertinencia = [[]]
+                for u in universo_discurso_letra_b:
+                    grau_pertinencia[0].append(conjunto_fuzzy_letra_b[c][str(u)])
+
+                desenha_grafico(grau_pertinencia, universo_discurso_letra_b)
+            # ---------------------------- #
+
+            # Letra c) ------------------- #
+            chaves = conjunto_fuzzy_letra_c.keys()
+
+            for c in chaves:
+                titulo = 'T-norma ' + c
+                grau_pertinencia = [[]]
+                for u in universo_discurso_letra_c:
+                    grau_pertinencia[0].append(conjunto_fuzzy_letra_c[c][str(u)])
+
+                desenha_grafico(grau_pertinencia, universo_discurso_letra_c)
             # # ---------------------------- #
-            #
-            # # Letra c) ------------------- #
-            # chaves = conjunto_fuzzy_letra_c.keys()
-            #
-            # for c in chaves:
-            #     titulo = 'T-norma ' + c
-            #     grau_pertinencia = [[]]
-            #     for u in universo_discurso_letra_c:
-            #         grau_pertinencia[0].append(conjunto_fuzzy_letra_c[c][str(u)])
-            #
-            #     desenha_grafico(grau_pertinencia, universo_discurso_letra_c)
-            # # # ---------------------------- #
-            #
-            # # Letra d) ------------------- #
-            # chaves = conjunto_fuzzy_letra_d.keys()
-            # chaves.sort()
-            #
-            # for c in chaves:
-            #     titulo = 'Complemento de um da funcao ' + c
-            #     grau_pertinencia = [[]]
-            #     for u in universo_discurso_letra_d:
-            #         grau_pertinencia[0].append(conjunto_fuzzy_letra_d[c][str(u)])
-            #
-            #     desenha_grafico(grau_pertinencia, universo_discurso_letra_d)
-            # # ---------------------------- #
+
+            # Letra d) ------------------- #
+            chaves = conjunto_fuzzy_letra_d.keys()
+            chaves.sort()
+
+            for c in chaves:
+                titulo = 'Complemento de um da funcao ' + c
+                grau_pertinencia = [[]]
+                for u in universo_discurso_letra_d:
+                    grau_pertinencia[0].append(conjunto_fuzzy_letra_d[c][str(u)])
+
+                desenha_grafico(grau_pertinencia, universo_discurso_letra_d)
+            # ---------------------------- #
 
             # Letra e) ------------------- #
             chaves = conjunto_fuzzy_letra_e.keys()
@@ -200,6 +200,174 @@ def main():
                     grau_pertinencia[0].append(conjunto_fuzzy_letra_e[c][str(u)])
 
                 desenha_grafico(grau_pertinencia, universo_discurso_letra_e)
+            # ---------------------------- #
+        elif opcao == '8':
+            # Questao 8 ------------------ #
+            print '\n----: QUESTAO 8 :----'
+            min_U = 0
+            max_U = 10
+
+            # Cria valor de incremento de acordo com o total de pontos da discretizacao
+            pontos = 500.0
+            discretizacao = (max_U - min_U) / pontos
+            # Cria lista com 20 pontos de discretizacao no intervalo do universo de discurso
+            universo_discurso = np.arange(min_U, max_U + discretizacao, discretizacao)
+
+            f = funcao_triangular(universo_discurso, 4.0, 5.0, 6.0)
+            g = funcao_triangular(universo_discurso, 3.0, 5.0, 7.0)
+            h = funcao_trapezoidal(universo_discurso, 1.0, 3.0, 4.0, 6.0)
+
+            # Letra a) ------------------------------------------------------- #
+            titulo = str(int(pontos)) + ' pontos de discretizacao [Letra a)]'
+
+            grau_pertinencia = [[], [], [], [], []]
+
+            for c in universo_discurso:
+                grau_pertinencia[0].append(f[str(c)])
+                grau_pertinencia[1].append(g[str(c)])
+                grau_pertinencia[2].append(h[str(c)])
+
+            desenha_grafico(grau_pertinencia)
+            # Letra a) ------------------------------------------------------- #
+
+            # Letra b) ------------------------------------------------------- #
+            if conjunto_eh_convexo(universo_discurso, f):
+                print 'Conjunto F e convexo!'
+            else:
+                print 'Conjunto F nao e convexo!'
+            if conjunto_eh_convexo(universo_discurso, g):
+                print 'Conjunto G e convexo!'
+            else:
+                print 'Conjunto G nao e convexo!'
+            if conjunto_eh_convexo(universo_discurso, h):
+                print 'Conjunto H e convexo!'
+            else:
+                print 'Conjunto H nao e convexo!'
+            # Letra b) ------------------------------------------------------- #
+
+            # Letra c) ------------------------------------------------------- #
+            conjunto_fuzzy = intersecao(f, g, False)
+
+            conjunto_fuzzy['minimo'] = intersecao(conjunto_fuzzy['minimo'], h, False)['minimo']
+            conjunto_fuzzy['produto'] = intersecao(conjunto_fuzzy['produto'], h, False)['produto']
+            conjunto_fuzzy['lukasiewicz'] = intersecao(conjunto_fuzzy['lukasiewicz'], h, False)['lukasiewicz']
+            conjunto_fuzzy['produto_drastico'] = intersecao(conjunto_fuzzy['produto_drastico'], h, False)['produto_drastico']
+
+            chaves = conjunto_fuzzy.keys()
+            chaves.sort()
+
+            for c in chaves:
+                titulo = 'Agregacao do tipo: ' + c.upper() + ' [Letra c)]'
+                grau_pertinencia = [[]]
+                for u in universo_discurso:
+                    grau_pertinencia[0].append(conjunto_fuzzy[c][str(u)])
+
+                desenha_grafico(grau_pertinencia, universo_discurso)
+            # Letra c) ------------------------------------------------------- #
+
+            # Letra d) ------------------------------------------------------- #
+            conjunto_fuzzy = uniao(f, g, False)
+
+            conjunto_fuzzy['maximo'] = uniao(conjunto_fuzzy['maximo'], h, False)['maximo']
+            conjunto_fuzzy['soma_probabilistica'] = uniao(conjunto_fuzzy['soma_probabilistica'], h, False)['soma_probabilistica']
+            conjunto_fuzzy['lukasiewicz'] = uniao(conjunto_fuzzy['lukasiewicz'], h, False)['lukasiewicz']
+            conjunto_fuzzy['soma_drastica'] = uniao(conjunto_fuzzy['soma_drastica'], h, False)['soma_drastica']
+
+            chaves = conjunto_fuzzy.keys()
+            chaves.sort()
+
+            for c in chaves:
+                titulo = 'Agregacao do tipo: ' + c.upper() + ' [Letra d)]'
+                grau_pertinencia = [[]]
+                for u in universo_discurso:
+                    grau_pertinencia[0].append(conjunto_fuzzy[c][str(u)])
+
+                desenha_grafico(grau_pertinencia, universo_discurso)
+            # Letra d) ------------------------------------------------------- #
+
+            # Letra e) ------------------------------------------------------- #
+            print 'Cardinalidade do conjunto F = ' + str( cardinalidade(f) )
+            print 'Cardinalidade do conjunto G = ' + str( cardinalidade(g) )
+            print 'Cardinalidade do conjunto H = ' + str( cardinalidade(h) )
+            # Letra e) ------------------------------------------------------- #
+
+            # Letra f) ------------------------------------------------------- #
+            print 'F contido em G? -> ' + inclusao(f,g)
+            print 'F contido em H? -> ' + inclusao(f,h)
+            print 'G contido em F? -> ' + inclusao(g,f)
+            print 'G contido em H? -> ' + inclusao(g,h)
+            print 'H contido em F? -> ' + inclusao(h,f)
+            print 'H contido em G? -> ' + inclusao(h,g)
+            # Letra f) ------------------------------------------------------- #
+
+            # Letra g) ------------------------------------------------------- #
+            print 'F contido em G? -> ' + igualdade(f,g)
+            print 'G contido em F? -> ' + igualdade(g,f)
+            print 'H contido em F? -> ' + igualdade(h,f)
+            print 'H contido em G? -> ' + igualdade(h,g)
+            # Letra g) ------------------------------------------------------- #
+
+            # Letra h) ------------------------------------------------------- #
+            # Resposta na letra g).
+            # Letra h) ------------------------------------------------------- #
+
+            # Letra i) ------------------------------------------------------- #
+            # Resposta na letra f).
+            # Letra i) ------------------------------------------------------- #
+
+            # Letra j) ------------------------------------------------------- #
+            conjunto_fuzzy = agregacao(f, g, False)
+
+            conjunto_fuzzy['aritmetica'] = agregacao(conjunto_fuzzy['aritmetica'], h, False)['aritmetica']
+            conjunto_fuzzy['harmonica'] = agregacao(conjunto_fuzzy['harmonica'], h, False)['harmonica']
+            conjunto_fuzzy['geometrica'] = agregacao(conjunto_fuzzy['geometrica'], h, False)['geometrica']
+            conjunto_fuzzy['minimo'] = agregacao(conjunto_fuzzy['minimo'], h, False)['minimo']
+            conjunto_fuzzy['maximo'] = agregacao(conjunto_fuzzy['maximo'], h, False)['maximo']
+
+            chaves = conjunto_fuzzy.keys()
+            chaves.sort()
+
+            for c in chaves:
+                titulo = 'Agregacao do tipo: ' + c.upper() + ' [Letra j)]'
+                grau_pertinencia = [[]]
+                for u in universo_discurso:
+                    grau_pertinencia[0].append(conjunto_fuzzy[c][str(u)])
+
+                desenha_grafico(grau_pertinencia, universo_discurso)
+            # Letra j) ------------------------------------------------------- #
+
+            # Letra k) ------------------------------------------------------- #
+            conjunto_fuzzy = {}
+
+            conjunto_fuzzy['f1'] = complemento(f, 0.3, -0.5, 2.0, False)
+            conjunto_fuzzy['f2'] = complemento(f, 0.75, 1.0, 1.0, False)
+            del conjunto_fuzzy['f2']['padrao']
+            del conjunto_fuzzy['f2']['yager']
+
+            conjunto_fuzzy['g1'] = complemento(g, 0.3, -0.5, 2.0, False)
+            conjunto_fuzzy['g2'] = complemento(g, 0.75, 1.0, 1.0, False)
+            del conjunto_fuzzy['g2']['padrao']
+            del conjunto_fuzzy['g2']['yager']
+
+            conjunto_fuzzy['h1'] = complemento(h, 0.3, -0.5, 2.0, False)
+            conjunto_fuzzy['h2'] = complemento(h, 0.75, 1.0, 1.0, False)
+            del conjunto_fuzzy['h2']['padrao']
+            del conjunto_fuzzy['h2']['yager']
+
+
+            for chave in conjunto_fuzzy:
+                chaves = conjunto_fuzzy[chave].keys()
+                chaves.sort()
+
+                for c in chaves:
+                    titulo = 'Complemento ' + c.upper() + ' da funcao ' + chave[0].upper() + ' [Letra k)]'
+                    grau_pertinencia = [[]]
+                    for u in universo_discurso:
+                        grau_pertinencia[0].append(conjunto_fuzzy[chave][c][str(u)])
+
+                    desenha_grafico(grau_pertinencia, universo_discurso)
+            # Letra k) ------------------------------------------------------- #
+
             # ---------------------------- #
         else:
         	print 'Finalizando execucao...'
@@ -300,7 +468,12 @@ def complemento(conjunto_fuzzy, a, l, w, return_msg=True):
         print 'Sugeno: ' + imprime_conjunto_fuzzy( conjunto_fuzzy_sugeno )
         print 'Yager: ' + imprime_conjunto_fuzzy( conjunto_fuzzy_yager )
     else:
-        return conjunto_fuzzy_padrao
+        resultado = {}
+        resultado['padrao'] = conjunto_fuzzy_padrao
+        resultado['threshold'] = conjunto_fuzzy_threshold
+        resultado['sugeno'] = conjunto_fuzzy_sugeno
+        resultado['yager'] = conjunto_fuzzy_yager
+        return resultado
 
 
 def agregacao(conjunto_fuzzy_1, conjunto_fuzzy_2, return_msg=True):
@@ -426,6 +599,73 @@ def funcao_gaussiana(universo_discurso, m, gama):
         conjunto_fuzzy[str(x)] = math.exp(-1 * potencial)
 
     return conjunto_fuzzy
+
+
+def conjunto_eh_convexo(universo_discurso, conjunto_fuzzy):
+    for x1 in universo_discurso:
+        for x2 in universo_discurso:
+            # Cria valor de incremento de acordo com o total de pontos da discretizacao
+            discretizacao = 1.0 / 20.0
+            # Cria lista com 20 pontos de discretizacao no intervalo do universo de discurso
+            lambda_ = np.arange(0.0, 1.0 + discretizacao, discretizacao)
+            for l in lambda_:
+                chave = str( (l*x1) + ((1-l)*x2) )
+                if chave in conjunto_fuzzy.keys():
+                    if conjunto_fuzzy[chave] < min(conjunto_fuzzy[str(x1)], conjunto_fuzzy[str(x2)]):
+                        return False
+
+    return True
+
+
+def cardinalidade(conjunto_fuzzy):
+    cardinalidade = 0.0
+    for chave in conjunto_fuzzy.keys():
+        cardinalidade += conjunto_fuzzy[chave]
+
+    return cardinalidade
+
+
+def igualdade(conjunto_fuzzy_1, conjunto_fuzzy_2):
+    msg = ''
+    grau_de_igualdade = 0.0
+
+    for chave in conjunto_fuzzy_1:
+        if conjunto_fuzzy_1[chave] != conjunto_fuzzy_2[chave]:
+            if abs(conjunto_fuzzy_1[chave] - conjunto_fuzzy_2[chave]) > grau_de_igualdade:
+                grau_de_igualdade = abs(conjunto_fuzzy_1[chave] - conjunto_fuzzy_2[chave])
+
+    if grau_de_igualdade == 0.0:
+        msg += 'Os conjuntos sao iguais. '
+    else:
+        msg += 'Os conjuntos nao sao iguais. '
+
+    msg += 'Grau de igualdade = ' + str(1 - grau_de_igualdade)
+
+    return msg
+
+
+def inclusao(conjunto_fuzzy_1, conjunto_fuzzy_2):
+    msg = ''
+    grau_de_inclusao = 0.0
+
+    for chave in conjunto_fuzzy_1:
+        if conjunto_fuzzy_1[chave] <= conjunto_fuzzy_2[chave]:
+            grau_de_inclusao += 1.0
+        else:
+            grau_de_inclusao += 1.0 - ( conjunto_fuzzy_1[chave] - conjunto_fuzzy_2[chave] )
+
+    print grau_de_inclusao
+    print len(universo_discurso)
+    grau_de_inclusao = grau_de_inclusao / len(conjunto_fuzzy_1.keys())
+
+    if grau_de_inclusao == 1.0:
+        msg += 'O conjunto 1 esta incluido no conjunto 2. '
+    else:
+        msg += 'O conjunto 1 nao esta incluido no conjunto 2. '
+
+    msg += 'Grau de inclusao = ' + str(grau_de_inclusao)
+
+    return msg
 
 
 def desenha_grafico(grau_pertinencia, universo_discurso_diferente=False):
